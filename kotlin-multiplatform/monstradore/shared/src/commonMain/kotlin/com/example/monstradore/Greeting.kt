db583f647d6d6e65f828ad440433e9e212debbe6
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-10-29 16:14:31 +0200

    set up jetpack compose for android

--- kotlin-multiplatform/monstradore/shared/src/commonMain/kotlin/com/example/monstradore/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.example.monstradore
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- kotlin-multiplatform/monstradore/shared/src/commonMain/kotlin/com/example/monstradore/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.example.monstradore
-
-expect class Platform() {
-    val platform: String
-}
