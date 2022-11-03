bcab9ed77c78f36c047cf5e503631b499279e79b
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-03 10:12:01 +0100

    create appbars

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/uiux/AndroidElementsView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/uiux/AndroidElementsView.kt
@@ -1,12 +1,22 @@
 package com.example.monstradore.android.uiux
 
+import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AppBars() {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Top App Bar") }
+            )
+        },
+        bottomBar = {
+            BottomAppBar{ }
+        }
     ) {
 
     }
