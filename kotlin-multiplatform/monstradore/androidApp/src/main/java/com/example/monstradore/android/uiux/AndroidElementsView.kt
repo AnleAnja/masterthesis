4947edc32190d5d0c4c1ba1b5dbcc44286dd3a0d
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-04 13:12:28 +0100

    add remaining android specific elements

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/uiux/AndroidElementsView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/uiux/AndroidElementsView.kt
@@ -1,13 +1,40 @@
 package com.example.monstradore.android.uiux
 
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @Composable
-fun AppBars() {
+fun AndroidElementsContent() {
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = "",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             TopAppBar(
@@ -16,8 +43,148 @@ fun AppBars() {
         },
         bottomBar = {
             BottomAppBar{ }
+        },
+        drawerContent = {
+            Text(
+                text = "Navigation Drawer Content",
+                modifier = Modifier.padding(20.dp),
+                textAlign = TextAlign.Center
+            )
+        },
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Text(
+                    text = "Floating Action Button",
+                    modifier = Modifier.padding(5.dp)
+                )
+            }
         }
     ) {
-
+        Column(
+            modifier = Modifier
+                .padding(5.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = "Card",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp),
+                elevation = 10.dp
+            ) {
+                Text(
+                    "Card Content",
+                    textAlign = TextAlign.Center
+                )
+            }
+            Text(
+                text = "Chip",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                "Nicht nativ verfügbar",
+                color = Color.Red
+            )
+            Text(
+                text = "Navigation Rail",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            NavigationRail {
+                NavigationRailItem(
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Rounded.Home,
+                            contentDescription = "1. Button"
+                        )
+                    },
+                    onClick = {},
+                    selected = false
+                )
+                NavigationRailItem(
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Rounded.Add,
+                            contentDescription = "2. Button"
+                        )
+                    },
+                    onClick = {},
+                    selected = false
+                )
+                NavigationRailItem(
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Rounded.AccountBox,
+                            contentDescription = "3. Button"
+                        )
+                    },
+                    onClick = {},
+                    selected = false
+                )
+            }
+            Text(
+                text = "Snackbar",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Button(onClick = {
+                scope.launch {
+                    scaffoldState.snackbarHostState
+                        .showSnackbar("Snackbar Content")
+                }
+            }) {
+                Text("Snackbar")
+            }
+            Text(
+                text = "Time Picker",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                "Nicht nativ verfügbar",
+                color = Color.Red
+            )
+            Text(
+                text = "Date Picker",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                "Nicht nativ verfügbar",
+                color = Color.Red
+            )
+            Text(
+                text = "Radio Buttons",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            var state by remember { mutableStateOf(true) }
+            Row(Modifier.selectableGroup()) {
+                RadioButton(
+                    selected = state,
+                    onClick = { state = true }
+                )
+                RadioButton(
+                    selected = !state,
+                    onClick = { state = false }
+                )
+            }
+            Text(
+                text = "Checkbox",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            val checkedState = remember { mutableStateOf(true) }
+            Checkbox(
+                checked = checkedState.value,
+                onCheckedChange = { checkedState.value = it }
+            )
+        }
     }
 }
