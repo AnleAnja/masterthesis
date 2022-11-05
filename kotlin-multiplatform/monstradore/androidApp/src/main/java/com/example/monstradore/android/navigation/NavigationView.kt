dac42b1906a26239292ec280166470b81b9a7295
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-05 15:05:23 +0100

    add bottom navigation (wip)

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/navigation/NavigationView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/navigation/NavigationView.kt
@@ -2,10 +2,9 @@ package com.example.monstradore.android.navigation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +12,12 @@ import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun NavigationContent() {
+    TabContent()
+    BottomNavigationContent()
+}
+
+@Composable
+fun TabContent() {
     var state by remember { mutableStateOf(0) }
     val titles = listOf("Tab 1", "Tab 2", "Tab 3")
     Column {
@@ -30,5 +35,22 @@ fun NavigationContent() {
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun BottomNavigationContent() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Songs", "Artists", "Playlists")
+
+    BottomNavigation {
+        items.forEachIndexed { index, item ->
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
     }
 }
