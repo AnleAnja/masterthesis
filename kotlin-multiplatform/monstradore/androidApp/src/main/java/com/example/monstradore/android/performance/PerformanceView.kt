6a8f57b0ff2e5a2b68d7a906bce9fb206241aa1f
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-09 13:30:38 +0100

    adjust text

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/performance/PerformanceView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/performance/PerformanceView.kt
@@ -11,9 +11,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.monstradore.structures.prime
+import kotlinx.coroutines.launch
 
 @Composable
 fun PerformanceContent() {
+    val coroutineScope = rememberCoroutineScope()
+
     var input by remember { mutableStateOf("20000") }
     var result by remember { mutableStateOf(0) }
     var time by remember { mutableStateOf(0L) }
@@ -25,17 +28,19 @@ fun PerformanceContent() {
             fontSize = 25.sp,
             fontWeight = FontWeight.Bold
         )
-        Text(text = "Die wievielte Primzahl soll berechnet werden:")
+        Text(text = "Die wievielte Primzahl soll berechnet werden?")
             TextField(
                 value = input,
                 onValueChange = { input = it },
                 label = { Text("Zahl eingeben") }
             )
             Button(onClick = {
-                val starttime = System.currentTimeMillis()
-                result = prime(input.toInt())
-                val endtime = System.currentTimeMillis()
-                time = (endtime - starttime) / 1000
+                coroutineScope.launch {
+                    val starttime = System.currentTimeMillis()
+                    result = prime(input.toInt())
+                    val endtime = System.currentTimeMillis()
+                    time = (endtime - starttime) / 1000
+                }
             }) {
                 Text("Berechnen")
         }
