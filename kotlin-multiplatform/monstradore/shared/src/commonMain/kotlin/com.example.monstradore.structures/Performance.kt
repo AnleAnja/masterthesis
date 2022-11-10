9cd9d750d871fefefab9aac9896c56ca59740171
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-10 01:04:15 +0100

    remove factorial

--- kotlin-multiplatform/monstradore/shared/src/commonMain/kotlin/com.example.monstradore.structures/Performance.kt
+++ kotlin-multiplatform/monstradore/shared/src/commonMain/kotlin/com.example.monstradore.structures/Performance.kt
@@ -1,16 +1,5 @@
 package com.example.monstradore.structures
 
-fun factorial(count: Long): Long {
-    if(count == 0L) return 0
-    var i = count-1
-    var result = count
-    while (i > 0) {
-        result *= i
-        i--
-    }
-    return result
-}
-
 fun prime(n: Int): Int {
     var num = 1
     var count=0
