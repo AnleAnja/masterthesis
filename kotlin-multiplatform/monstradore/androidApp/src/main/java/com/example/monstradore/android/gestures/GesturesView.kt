f22af4061f9a03da064e1832373328ca58e79aae
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-11 17:03:05 +0100

    add zoom on double tap

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/gestures/GesturesView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/gestures/GesturesView.kt
@@ -1,24 +1,16 @@
 package com.example.monstradore.android.gestures
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.monstradore.android.R
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.roundToInt
-import kotlin.math.sin
 
 @Composable
 fun GesturesContent() {
@@ -27,9 +19,18 @@ fun GesturesContent() {
 
 @Composable
 fun ImagePreview(model: Any, contentDescription: String? = null) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        var zoom by remember { mutableStateOf(1f) }
-
+    var zoom by remember { mutableStateOf(1f) }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onDoubleTap = {
+                    zoom = if(zoom == 1f) 4f
+                    else 1f
+                }
+            )
+        }
+    ) {
         AsyncImage(
             model,
             contentDescription = contentDescription,
