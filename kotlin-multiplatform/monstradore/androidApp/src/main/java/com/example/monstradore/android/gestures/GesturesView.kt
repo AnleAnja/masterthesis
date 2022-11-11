61e1dd182ca1c7a14ccf25a88e96458e6c71bfd9
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-11 16:47:16 +0100

    add gesture functionality

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/gestures/GesturesView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/gestures/GesturesView.kt
@@ -1,7 +1,52 @@
 package com.example.monstradore.android.gestures
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.monstradore.android.R
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.roundToInt
+import kotlin.math.sin
 
 @Composable
 fun GesturesContent() {
+    ImagePreview(model = R.drawable.sample, "")
+}
+
+@Composable
+fun ImagePreview(model: Any, contentDescription: String? = null) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        var zoom by remember { mutableStateOf(1f) }
+
+        AsyncImage(
+            model,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .graphicsLayer(
+                    scaleX = zoom,
+                    scaleY = zoom
+                )
+                .pointerInput(Unit) {
+                    detectTransformGestures(
+                        onGesture = { _, _, gestureZoom, _ ->
+                            zoom = (zoom * gestureZoom).coerceIn(1F..4F)
+                        }
+                    )
+                }
+                .fillMaxSize()
+        )
+    }
 }
