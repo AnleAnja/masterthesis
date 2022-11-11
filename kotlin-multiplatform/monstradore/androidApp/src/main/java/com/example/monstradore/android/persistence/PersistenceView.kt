b9217df9f68589a409ee36104c0e9e124a0263e7
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-11 10:56:14 +0100

    optimize imports

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/persistence/PersistenceView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/persistence/PersistenceView.kt
@@ -1,7 +1,6 @@
 package com.example.monstradore.android.persistence
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -11,7 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.monstradore.storage.*
+import com.example.monstradore.storage.UserStorage
 
 @Composable
 fun PersistenceContent(
