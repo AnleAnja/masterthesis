8cc815de69ccf6a5dc6261d31f541ee0ddadd826
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-12 13:53:56 +0100

    add video player

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/multimedia/MultimediaView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/multimedia/MultimediaView.kt
@@ -1,7 +1,7 @@
 package com.example.monstradore.android.multimedia
 
 import android.media.MediaPlayer
-import androidx.compose.foundation.background
+import android.net.Uri
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -10,13 +10,22 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.monstradore.android.R
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+
 
 @Composable
 fun MultimediaContent() {
@@ -43,7 +52,7 @@ fun AudioContent() {
     val context = LocalContext.current
     val mediaPlayer = MediaPlayer.create(
         context,
-        R.raw.sample
+        R.raw.sampleaudio
     )
     Row {
         IconButton(onClick = { mediaPlayer.start() }) {
@@ -58,14 +67,17 @@ fun AudioContent() {
 @Composable
 fun VideoContent() {
     val context = LocalContext.current
-    val mediaPlayer = MediaPlayer.create(
-        context,
-        R.raw.video
-    )
-    Column {
-        mediaPlayer.setVideoScalingMode(1)
-        IconButton(onClick = { mediaPlayer.start() }) {
-            Icon(Icons.Outlined.PlayArrow, contentDescription = "")
+    val url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    val exoPlayer = ExoPlayer.Builder(context).build()
+    val source = MediaItem.fromUri(Uri.parse(url))
+    exoPlayer.setMediaItem(source)
+    val playerView = StyledPlayerView(context)
+    playerView.player = exoPlayer
+    DisposableEffect(AndroidView(factory = {playerView})) {
+        exoPlayer.prepare()
+        exoPlayer.playWhenReady = true
+        onDispose {
+            exoPlayer.release()
         }
     }
 }
