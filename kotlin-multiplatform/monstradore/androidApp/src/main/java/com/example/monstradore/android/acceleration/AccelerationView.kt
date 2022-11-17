f66038f78a517fb538ba38c8d704b0fe06b6b343
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-17 14:40:08 +0100

    add acceleration events

--- kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/acceleration/AccelerationView.kt
+++ kotlin-multiplatform/monstradore/androidApp/src/main/java/com/example/monstradore/android/acceleration/AccelerationView.kt
@@ -2,9 +2,13 @@ package com.example.monstradore.android.acceleration
 
 import android.content.Context.SENSOR_SERVICE
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -17,7 +21,18 @@ import androidx.compose.ui.unit.sp
 fun AccelerationContent() {
     val sensorManager = LocalContext.current.getSystemService(SENSOR_SERVICE) as SensorManager
     val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-    var sensorState by remember { mutableStateOf(sensor) }
+    var sensorState = remember { mutableStateOf("") }
+    sensorManager.registerListener(object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.values?.forEach {
+                sensorState.value = it.toString()
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+        }
+    }, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     Column(
         modifier = Modifier.padding(10.dp)
     ) {
@@ -26,11 +41,6 @@ fun AccelerationContent() {
             fontSize = 25.sp,
             fontWeight = FontWeight.Bold
         )
-        /*Button(onClick = {
-            sensorState = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        }) {
-            Text("Aktualisieren")
-        }*/
-        Text(sensorState.toString())
+        Text(sensorState.value)
     }
 }
