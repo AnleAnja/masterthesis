293247db9e15455f8e23859ec36be11e62ad79c6
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-21 13:42:18 +0100

    create new structs for elements

--- kotlin-multiplatform/monstradore/iosApp/iosApp/uiux/UIElementsView.swift
+++ kotlin-multiplatform/monstradore/iosApp/iosApp/uiux/UIElementsView.swift
@@ -13,58 +13,24 @@ struct UIElementsView: View {
     @State private var isEditing = false
     var body: some View {
         ScrollView {
-            VStack
-//            (
-////                alignment: .leading
-//            )
+            VStack(alignment: .leading)
             {
-                Text("Grundlegende Elemente")
-                    .font(.title)
-                Text("Text")
-                    .font(.headline)
-                Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.")
-                Text("Bild")
-                    .font(.headline)
-                Image("sample")
-                Text("Liste")
-                    .font(.headline)
-                List {
-                    Text("Item: 0")
-                    Text("Item: 1")
-                    Text("Item: 2")
-                    Text("Item: 3")
-                    Text("Item: 4")
-                }
-                Text("Button")
-                    .font(.headline)
-                Button(action: {}) {
-                    Text("Button")
-                }
-                Text("Icon Button")
-                    .font(.headline)
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Image(systemName: "pencil")
-                }
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Label("Edit", systemImage: "pencil")
-                }
+                BasicElements()
+                ButtonElements()
                 Text("Elemente mit Statusverwaltung")
                     .font(.title)
                 Text("Slider")
                     .font(.headline)
                 Slider(
-                            value: $speed,
-                            in: 0...100,
-                            onEditingChanged: { editing in
-                                isEditing = editing
-                            }
-                        )
+                    value: $speed,
+                    in: 0...100,
+                    onEditingChanged: { editing in
+                        isEditing = editing
+                    }
+                )
             }
             .padding()
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
@@ -74,3 +40,47 @@ struct UIElementsView: View {
 //        UIElementsView()
 //    }
 //}
+
+struct BasicElements: View {
+    var body: some View {
+        Text("Grundlegende Elemente")
+            .font(.title)
+        Text("Text")
+            .font(.headline)
+        Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.")
+        Text("Bild")
+            .font(.headline)
+        Image("sample")
+        Text("Liste")
+            .font(.headline)
+        List {
+            Text("Item: 0")
+            Text("Item: 1")
+            Text("Item: 2")
+            Text("Item: 3")
+            Text("Item: 4")
+        }
+    }
+}
+
+struct ButtonElements: View {
+    var body: some View {
+        Text("Button")
+            .font(.headline)
+        Button(action: {}) {
+            Text("Button")
+        }
+        Text("Icon Button")
+            .font(.headline)
+        Button {
+            print("Edit button was tapped")
+        } label: {
+            Image(systemName: "pencil")
+        }
+        Button {
+            print("Edit button was tapped")
+        } label: {
+            Label("Edit", systemImage: "pencil")
+        }
+    }
+}
