295f6c246bca536cc131bf78c06149bb029812be
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-28 21:35:11 +0100

    change navigation from overview

--- kotlin-multiplatform/monstradore/iosApp/iosApp/OverviewView.swift
+++ kotlin-multiplatform/monstradore/iosApp/iosApp/OverviewView.swift
@@ -10,6 +10,17 @@ extension String: Identifiable {
 
 struct OverviewView: View {
     let categories = Features.shared.overview
+    
+    @ViewBuilder
+    func element(_ feature: String, _ desired: String, _ dest: some View) -> some View {
+        if(feature == desired) {
+            NavigationLink(destination: dest) {
+                Text(feature)
+                    .font(.body)
+            }
+        }
+    }
+    
     var body: some View {
         NavigationView {
             List {
@@ -19,69 +30,27 @@ struct OverviewView: View {
                             .font(.headline)
                     ){
                         ForEach(category.features) { feature in
-                            if(feature == "Reichhaltige UI Elemente") {
-                                NavigationLink(destination: UIElementsView()) {
-                                    Text(feature)
-                                        .font(.body)
-                                }
-                            }
-                            if(feature == "Interaktionsdesign") {
-                                NavigationLink(destination: InteractionDesignView()) {
-                                    Text(feature)
-                                        .font(.body)
-                                }
-                            }
-                            if(feature == "Gesten") {
-                                NavigationLink(destination: GestureView()) {
-                                Text(feature)
-                                        .font(.body)
-                                }
-                            }
-                            if(feature == "Navigation") {
-                                NavigationLink(destination: NavigationElementsView()) {
-                                    Text(feature)
-                                        .font(.body)
-                                }
-                          }
-                            if(feature == "Eingabemethoden") {
-                                NavigationLink(destination: InputMethodsView()) {
-                                    Text(feature)
-                                        .font(.body)
-                                }
-                           }
-                          if(feature == "Multimedia") {
-                                NavigationLink(destination: MultimediaView()) {
-                                    Text(feature)
-                                        .font(.body)
-                                }
-                          }
-                          if(feature == "Animationen") {
-                                NavigationLink(destination: AnimationsView()) {
-                                    Text(feature)
-                                        .font(.body)
-                                }
-                         }
-                         if(feature == "Netzwerkcalls") {
-                                NavigationLink(destination: NetworkCallView()) {
-                                Text(feature)
-                                        .font(.body)
-                                }
-                         }
-                         if(feature == "Dateizugriff") {
-                                NavigationLink(destination: FileAccessView()) {
-                                Text(feature)
-                                        .font(.body)
-                                }
-                         }
-                         if(feature == "Persistierung") {
-                                NavigationLink(destination: PersistenceView()) {
-                                    Text(feature)
-                                        .font(.body)
-                                }
-                         }
+                            element(feature, "Reichhaltige UI Elemente", UIElementsView())
+                            element(feature, "Interaktionsdesign", InteractionDesignView())
+                            element(feature, "Gesten", GestureView())
+                            element(feature, "Navigation", NavigationElementsView())
+                            element(feature, "Eingabemethoden", InputMethodsView())
+                            element(feature, "Multimedia", MultimediaView())
+                            element(feature, "Animationen", AnimationsView())
+                            //element(feature, "2D und 3D Grafiken", 3DGraphicsView())
+                            element(feature, "Netzwerkcalls", NetworkCallView())
+                            element(feature, "Dateizugriff", FileAccessView())
+                            element(feature, "Persistierung", PersistenceView())
+                            //element(feature, "Zugriff auf native Anwendungen", AppAccessView())
+                            //element(feature, "Kamera", CameraView())
+                            //element(feature, "GPS", GPSView())
+                            //element(feature, "Beschleunigung", AccelerationView())
+                            //element(feature, "Fingerabdruck / Face ID", FingerprintView())
+                            //element(feature, "Primzahlberechnung", PerformanceView())
+                        }
                     }
+                    .navigationTitle("monstradore")
                 }
-                .navigationTitle("monstradore")
             }
         }
     }
