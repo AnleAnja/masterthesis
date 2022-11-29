5d18b233fba48220a3cab999cfc828bfec6a6ec5
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-29 01:30:17 +0100

    set up gps sensor (wip)

---
+++ kotlin-multiplatform/monstradore/iosApp/iosApp/gps/GPSView.swift
@@ -0,0 +1,38 @@
+//
+//  GPSView.swift
+//  iosApp
+//
+//  Created by Anja on 29.11.22.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct GPSView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Location")
+                .font(.headline)
+            Text("location status: \(locationManager.statusString)")
+            HStack {
+                Text("Latitude: \(userLatitude), Longitude \(userLongitude)")
+            }
+        }
+    }
+}
+
+//struct GPSView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        GPSView()
+//    }
+//}
