2d6a5f64ad020f70768469893f3b75ed76f46827
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-11-30 15:35:26 +0100

    restructure data

---
+++ kotlin-multiplatform/monstradore/iosApp/iosApp/objects/ObjectView.swift
@@ -0,0 +1,27 @@
+//
+//  ObjectView.swift
+//  iosApp
+//
+//  Created by Anja on 29.11.22.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+import SceneKit
+
+struct ObjectView: View {
+    var body: some View {
+        VStack {
+            Text("3D Objekte")
+                .font(.headline)
+            SceneView(scene: SCNScene(named: "NHMW-Zoo1-HS-23783-Komodowaran_low res.obj"), options: [.autoenablesDefaultLighting, .allowsCameraControl])
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.8)
+        }
+    }
+}
+
+//struct ObjectView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ObjectView()
+//    }
+//}
