d83e6f08e3a7d3921aa9fe1002daf63309a8af62
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-12-11 15:09:53 +0100

    add pinch and double tap to zoom

--- flutter/monstradore/lib/main.dart
+++ flutter/monstradore/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grouped_list/grouped_list.dart';
+import 'package:monstradore/gestures/gestures.dart';
 
 void main() {
   runApp(const MyApp());
@@ -78,14 +79,28 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           itemBuilder: (c, element) {
-            return Container(
+            return GestureDetector(
+                onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  switch (element['name']) {
+                    case 'Gesten':
+                      return const Gestures();
+                    default:
+                      return const Text("Unbekanntes Feature");
+                  }
+                }),
+              );
+            },
+            child: Container(
               margin: const EdgeInsets.symmetric(horizontal: 10.0,
                   vertical: 10.0),
               child: Text(
                   element['name'],
                   style: const TextStyle(fontSize: 16),
                 ),
-            );
+            ));
           },
         ),
         );
