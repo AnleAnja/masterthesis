a3944d900eef52c0d1ea103992223d9b0ab44e8b
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-12-12 15:03:18 +0100

    add animations

--- flutter/monstradore/lib/animations/animations.dart
+++ flutter/monstradore/lib/animations/animations.dart
@@ -8,24 +8,61 @@ class Animations extends StatefulWidget {
   State<StatefulWidget> createState() => _AnimationsState();
 }
 
-class _AnimationsState extends State<Animations> {  @override
+class _AnimationsState extends State<Animations> {
+  double size = 128.0;
+  bool large = false;
+  bool visible = true;
+  String hideShowText = "Hide";
 
+  @override
   Widget build(BuildContext context) {
-  final theme = Theme.of(context);
-  final textTheme = theme.textTheme;
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Animationen", style: textTheme.headline6),
-          TextButton(onPressed: () {
+        body: Center(
+            child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+          child: Text("Animationen", style: textTheme.headline6),
+        ),
+        TextButton(
+            onPressed: () => updateSize(), child: const Text("Transition")),
+        AnimatedSize(
+                curve: Curves.easeIn,
+                duration: const Duration(seconds: 1),
+                child: Container(
+                  color: Colors.grey,
+                  height: size,
+                  width: size,
+                )),
+        TextButton(onPressed: () {
+          setState(() {
+            visible = !visible;
+            if (visible) {
+              hideShowText = "Hide";
+            } else {
+              hideShowText = "Show";
+            }
+          });
+        }, child: Text(hideShowText)),
+        AnimatedOpacity(
+            opacity: visible ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 500),
+                child: Container(
+                  color: Colors.grey,
+                  height: 128.0,
+                  width: 128.0,
+                )),
+      ],
+    )));
+  }
 
-          }, child: const Text("Transition")),
-          TextButton(onPressed: () {
-
-          }, child: const Text("Hide")),
-        ],
-      )
-    );
+  void updateSize() {
+    setState(() {
+      size = large ? 128.0 : 64.0;
+      large = !large;
+    });
   }
 }
