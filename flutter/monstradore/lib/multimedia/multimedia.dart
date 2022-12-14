b169052b1cf29c9e2ce1909a443d8f3c23bf0005
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-12-14 12:46:03 +0100

    fix multimedia

--- flutter/monstradore/lib/multimedia/multimedia.dart
+++ flutter/monstradore/lib/multimedia/multimedia.dart
@@ -15,7 +15,10 @@ class _MultimediaState extends State<Multimedia> {
   bool isPlaying = false;
   bool audioPlayed = false;
   late Uint8List audioBytes;
+
+  // Video Player
   late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
 
   @override
   void initState() {
@@ -29,10 +32,21 @@ class _MultimediaState extends State<Multimedia> {
     });
 
     _controller = VideoPlayerController.network(
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
-      ..initialize().then((_) {
-        setState(() {});
-      });
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -97,12 +111,26 @@ class _MultimediaState extends State<Multimedia> {
           ],
         ),
         Text("Video", style: textTheme.headline6),
-        _controller.value.isInitialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                )
-              : Container(),
+        FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the VideoPlayerController has finished initialization, use
+              // the data it provides to limit the aspect ratio of the video.
+              return AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                // Use the VideoPlayer widget to display the video.
+                child: VideoPlayer(_controller),
+              );
+            } else {
+              // If the VideoPlayerController is still initializing, show a
+              // loading spinner.
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
         Row(
           children: [
             IconButton(
