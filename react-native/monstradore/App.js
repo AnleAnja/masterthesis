178d79571e251f63de2ecbde5a8f65f4181e584a
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-12-19 23:33:56 +0100

    add navigation for features

--- react-native/monstradore/App.js
+++ react-native/monstradore/App.js
@@ -1,12 +1,8 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  SectionList,
-  StatusBar,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView, SectionList} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import EmptyScreen from './EmptyScreen';
 
 const DATA = [
   {
@@ -41,40 +37,16 @@ const DATA = [
   },
 ];
 
-const Item = ({title}) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
-
-class App extends Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <SectionList
-          sections={DATA}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({item}) => <Item title={item} />}
-          renderSectionHeader={({section: {title}}) => (
-            <Text style={styles.header}>{title}</Text>
-          )}
-        />
-      </SafeAreaView>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: StatusBar.currentHeight,
     marginHorizontal: 16,
   },
   item: {
     padding: 5,
   },
   header: {
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: 'bold',
     padding: 5,
   },
@@ -83,5 +55,79 @@ const styles = StyleSheet.create({
     padding: 5,
   },
 });
+
+function OverviewScreen({navigation}) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text
+              onPress={() => {
+                if (item === 'Reichhaltige UI Elemente') {
+                  navigation.navigate();
+                } else if (item === 'Interaktionsdesign') {
+                  navigation.navigate();
+                } else if (item === 'Gesten') {
+                  navigation.navigate();
+                } else if (item === 'Navigation') {
+                  navigation.navigate();
+                } else if (item === 'Eingabemethoden') {
+                  navigation.navigate();
+                } else if (item === 'Multimedia') {
+                  navigation.navigate();
+                } else if (item === 'Animationen') {
+                  navigation.navigate();
+                } else if (item === '3D Grafiken') {
+                  navigation.navigate();
+                } else if (item === 'Netzwerkcalls') {
+                  navigation.navigate();
+                } else if (item === 'Dateizugriff') {
+                  navigation.navigate();
+                } else if (item === 'Persistierung') {
+                  navigation.navigate();
+                } else if (item === 'Zugriff auf native Anwendungen') {
+                  navigation.navigate();
+                } else if (item === 'Kamera') {
+                  navigation.navigate();
+                } else if (item === 'GPS') {
+                  navigation.navigate();
+                } else if (item === 'Beschleunigung') {
+                  navigation.navigate();
+                } else if (item === 'Fingerabdruck / Face ID') {
+                  navigation.navigate();
+                } else if (item === 'Primzahlberechnung') {
+                  navigation.navigate();
+                } else {
+                  navigation.navigate('Empty');
+                }
+              }}
+              style={styles.title}>
+              {item}
+            </Text>
+          </View>
+        )}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Overview" component={OverviewScreen} />
+        <Stack.Screen name="Empty" component={EmptyScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
---
+++ react-native/monstradore/EmptyScreen.js
@@ -0,0 +1,7 @@
+import {Text} from 'react-native';
+
+function EmptyScreen() {
+  return <Text>"Nicht nativ verfügbar"</Text>;
+}
+
+export default EmptyScreen;
