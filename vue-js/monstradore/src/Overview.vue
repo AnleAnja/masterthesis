7c57dde5c87e16f35c5fa763cb77aef778318be5
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-12-29 02:08:30 +0100

    overview setup content

---
+++ vue-js/monstradore/src/Overview.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  data() {
+  	return {
+      data: [
+      {
+        category: 'UI / UX',
+        items: [
+          { message: 'Reichhaltige UI Elemente' }, 
+          { message: 'Interaktionsdesign' }, 
+          { message: 'Gesten' }, 
+          { message: 'Navigation' }, 
+          { message: 'Eingabemethoden' }, 
+          { message: 'Multimedia' }, 
+          { message: 'Animationen' }, 
+          { message: '3D Grafiken' } 
+        ]
+      },
+      {
+        category: 'Gerätespezifische Funktionen',
+        items: [
+          { message: 'Netzwerkcalls' },
+      		{ message: 'Dateizugriff' },
+		      { message: 'Persistierung' },
+    		  { message: 'Zugriff auf native Anwendungen' },
+		      { message: 'Kamera' },
+    		  { message: 'GPS' },
+		      { message: 'Beschleunigung' },
+    		  { message: 'Fingerabdruck / Face ID' }
+        ]
+      },
+      {
+        category: 'Algorithmen',
+        items: [
+          { message: 'Primzahlberechnung' }
+        ]
+      }
+   ]}
+	}
+}
+</script>
+
+<template>
+	<li class="category" v-for="(element) in data">
+  	    {{ element.category }}
+        <li class="items" v-for="(item) in element.items">
+            {{ item.message }}
+        </li>
+	</li>
+</template>
+
+<style scoped>
+
+category {
+  font-weight: bold;
+  font-size: large;
+}
+
+items {
+    font-size: medium;
+}
+
+</style>
---
+++ vue-js/monstradore/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Overview from './Overview.vue'
+
+import './assets/main.css'
+
+createApp(Overview).mount('#app')
