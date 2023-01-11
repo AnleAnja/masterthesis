4fdb04df6529c91d9175dd4a6fa94ee1a8cb4102
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2023-01-11 13:43:14 +0100

    implement pinch to zoom

--- vue-js/monstradore/src/router/index.js
+++ vue-js/monstradore/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
   {
     path: "/2",
     name: "gestures",
-    component: () => import("../views/AboutView.vue"),
+    component: () => import("../views/GesturesView.vue"),
   },
   {
     path: "/3",
---
+++ vue-js/monstradore/src/views/GesturesView.vue
@@ -0,0 +1,35 @@
+import PinchZoom from 'vue-pinch-zoom';
+
+<template>
+  <div class="center">
+    <pinch-zoom>
+      <img class="img" src="../assets/sample.jpeg" alt="image" />
+    </pinch-zoom>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GesturesView",
+};
+</script>
+
+<style scoped>
+.center {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+}
+
+.img {
+  display: block;
+  margin: auto;
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
