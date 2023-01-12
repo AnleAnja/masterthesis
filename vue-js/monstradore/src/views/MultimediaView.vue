b11551bbb0a55594f6ea52fad40670893d8644dc
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2023-01-12 22:20:12 +0100

    add audio file

---
+++ vue-js/monstradore/src/views/MultimediaView.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Audio</h1>
+  <button @click="playSound">Say hello</button>
+  <h1>Video</h1>
+</template>
+
+<script>
+import sound from "../assets/sampleaudio.mp3";
+
+export default {
+  data() {
+    return {
+      name: "MultimediaView",
+    };
+  },
+  methods: {
+    playSound() {
+      const audio = new Audio(sound);
+      audio.play();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
