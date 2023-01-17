801acbfcc9d5d1c9bfe5e9541d8676db34882ef2
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2023-01-17 21:41:08 +0100

    add multithreading via worker

--- vue-js/monstradore/src/views/PerformanceView.vue
+++ vue-js/monstradore/src/views/PerformanceView.vue
@@ -3,7 +3,7 @@
   <p>Die wievielte Primzahl soll berechnet werden?</p>
   <v-text-field v-model.number="input" placeholder="Zahl eingeben" />
   <div>
-    <v-btn @click="calcPrime">Berechnen</v-btn>
+    <v-btn @click="calc">Berechnen</v-btn>
   </div>
   <p>Ergebnis: {{ result }}</p>
   <p>Benötigte Zeit: {{ time }} Sekunden</p>
@@ -11,34 +11,48 @@
 </template>
 
 <script>
+import worker from "./worker";
+const fn = worker.toString();
+const fnBody = fn.substring(fn.indexOf("{") + 1, fn.lastIndexOf("}"));
+const workerSourceURL = URL.createObjectURL(new Blob([fnBody]));
+const bgCalc = new Worker(workerSourceURL);
+
 export default {
   data() {
     return {
       name: "PerformanceView",
-      input: 20000,
+      input: "20000",
       result: 0,
       time: 0,
+      start: Date.now(),
     };
   },
   methods: {
-    calcPrime() {
-      let num = 1;
-      let count = 0;
-      while (count < this.input) {
-        num++;
-        let i = 2;
-        while (i <= num) {
-          if (num % i === 0) {
-            break;
-          }
-          i++;
-        }
-        if (i === num) {
-          count++;
-        }
+    calc() {
+      this.start = Date.now();
+      try {
+        const number = parseInt(this.input);
+        bgCalc.postMessage({
+          key: "calcPrime",
+          args: [number],
+        });
+      } catch (e) {
+        console.log(e);
       }
-      this.result = num;
     },
+  },
+  created() {
+    bgCalc.onmessage = (event) => {
+      if (event.data.key === "working") {
+        this.$emit("loading", event.data.value);
+      } else if (event.data.key === "nums") {
+        let end = Date.now();
+        this.time = Math.floor((end - this.start) / 1000);
+        this.result = event.data.value;
+      } else {
+        console.error(event.data);
+      }
+    };
   },
 };
 </script>
