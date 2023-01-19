f59f7a876d668023324c8765af2030b01e2d7aa2
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2023-01-19 22:29:26 +0100

    change acceleration access

--- vue-js/monstradore/src/views/AccelerationView.vue
+++ vue-js/monstradore/src/views/AccelerationView.vue
@@ -5,69 +5,66 @@
   <p>Roll: {{ this.acceleration.x }}</p>
   <p>Pitch: {{ this.acceleration.y }}</p>
   <p>Yaw: {{ this.acceleration.z }}</p>
+  <span>{{ this.log }}</span>
+  <p>{{ this.interval }}</p>
+  <!--UseDeviceMotion v-slot="{ accelerationIncludingGravity, interval }">
+    acceleration: {{ accelerationIncludingGravity }}, interval: {{ interval }}
+  </UseDeviceMotion-->
 </template>
 
 <script>
-import { Motion } from "@capacitor/motion";
+// import { Motion } from "@capacitor/motion";
+// import { UseDeviceMotion } from "@vueuse/components";
 
 export default {
+  // components: {
+  //   UseDeviceMotion,
+  // },
   data() {
     return {
       name: "AccelerationView",
       acceleration: {
-        x: 0,
-        y: 0,
-        z: 0,
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
       },
       accelHandler: null,
+      log: "testjkjh",
+      isRunning: false,
+      interval: "0.0",
     };
   },
   methods: {
-    async startAcceleration() {
-      if (window.DeviceMotionEvent) {
-        console.log("set acc handler");
-        window.addEventListener("devicemotion", this.handleMotion, true);
-        self.supported = true;
-      }
-      console.log(window.DeviceMotionEvent);
-
-      window.addEventListener(
-        "compassneedscalibration",
-        function (event) {
-          self.supported = true;
-          alert("Your compass needs calibrating!");
-          event.preventDefault();
-        },
-        true
-      );
-      // try {
-      //   await DeviceMotionEvent.requestPermission();
-      // } catch (e) {
-      //   // Handle error
-      //   console.log(e);
-      //   return;
+    startAcceleration(event) {
+      event.preventDefault();
+      console.log(window);
+      this.log = "start acceleration";
+      window.ondevicemotion = (e) => {
+        console.log(e);
+      };
+      // if (
+      //   window.DeviceMotionEvent &&
+      //   typeof window.DeviceMotionEvent.requestPermission === "function"
+      // ) {
+      //   this.log = "set acc handler";
+      //   window.DeviceMotionEvent.requestPermission();
       // }
       //
-      // // Once the user approves, can start listening:
-      // this.accelHandler = await Motion.addListener("accel", (event) => {
-      //   console.log("Device motion event:", event);
-      //   this.handleMotion(event);
-      // });
-
-      console.log(this.accelHandler);
+      // window.addEventListener("devicemotion", this.handleMotion);
+      // this.log = "event set";
     },
     handleMotion(event) {
+      console.log(event);
       this.acceleration.x = event.acceleration.x;
       this.acceleration.y = event.acceleration.y;
       this.acceleration.z = event.acceleration.z;
+      this.log = event.toString();
+      this.interval = event.interval.toString();
     },
     stopAcceleration() {
       if (this.accelHandler) {
         this.accelHandler.remove();
       }
-    },
-    removeListeners() {
-      Motion.removeAllListeners();
     },
   },
 };
