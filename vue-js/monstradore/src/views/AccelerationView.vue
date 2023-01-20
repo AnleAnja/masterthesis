485525c8e09601b0f9c7b3262dcdd1442869fc0a
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2023-01-20 15:42:04 +0100

    fix acceleration

--- vue-js/monstradore/src/views/AccelerationView.vue
+++ vue-js/monstradore/src/views/AccelerationView.vue
@@ -1,25 +1,13 @@
 <template>
-  <v-btn @click="startAcceleration">Start</v-btn>
-  <v-btn @click="stopAcceleration">Stop</v-btn>
+  <v-btn @click="requestPermissionAndStart">Request Permissions</v-btn>
   <h1>Beschleunigung</h1>
   <p>Roll: {{ this.acceleration.x }}</p>
   <p>Pitch: {{ this.acceleration.y }}</p>
   <p>Yaw: {{ this.acceleration.z }}</p>
-  <span>{{ this.log }}</span>
-  <p>{{ this.interval }}</p>
-  <!--UseDeviceMotion v-slot="{ accelerationIncludingGravity, interval }">
-    acceleration: {{ accelerationIncludingGravity }}, interval: {{ interval }}
-  </UseDeviceMotion-->
 </template>
 
 <script>
-// import { Motion } from "@capacitor/motion";
-// import { UseDeviceMotion } from "@vueuse/components";
-
 export default {
-  // components: {
-  //   UseDeviceMotion,
-  // },
   data() {
     return {
       name: "AccelerationView",
@@ -28,43 +16,34 @@ export default {
         y: 0.0,
         z: 0.0,
       },
-      accelHandler: null,
-      log: "testjkjh",
-      isRunning: false,
-      interval: "0.0",
     };
   },
+  created() {
+    this.startListening();
+  },
   methods: {
-    startAcceleration(event) {
-      event.preventDefault();
-      console.log(window);
-      this.log = "start acceleration";
-      window.ondevicemotion = (e) => {
-        console.log(e);
+    startListening() {
+      window.ondevicemotion = (event) => {
+        this.acceleration.x = event.acceleration.x;
+        this.acceleration.y = event.acceleration.y;
+        this.acceleration.z = event.acceleration.z;
       };
-      // if (
-      //   window.DeviceMotionEvent &&
-      //   typeof window.DeviceMotionEvent.requestPermission === "function"
-      // ) {
-      //   this.log = "set acc handler";
-      //   window.DeviceMotionEvent.requestPermission();
-      // }
-      //
-      // window.addEventListener("devicemotion", this.handleMotion);
-      // this.log = "event set";
     },
-    handleMotion(event) {
-      console.log(event);
-      this.acceleration.x = event.acceleration.x;
-      this.acceleration.y = event.acceleration.y;
-      this.acceleration.z = event.acceleration.z;
-      this.log = event.toString();
-      this.interval = event.interval.toString();
-    },
-    stopAcceleration() {
-      if (this.accelHandler) {
-        this.accelHandler.remove();
+    requestPermissionAndStart(e) {
+      e.preventDefault();
+
+      try {
+        if (
+          window.DeviceMotionEvent &&
+          typeof window.DeviceMotionEvent.requestPermission === "function"
+        ) {
+          window.DeviceMotionEvent.requestPermission();
+        }
+      } catch (e) {
+        console.log(e);
       }
+
+      this.startListening();
     },
   },
 };
