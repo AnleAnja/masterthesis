68f08f2fff40922fed3b87104b97ba8730cfb142
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2023-01-30 16:03:37 +0100

    wip

--- vue-js/monstradore/src/views/InteractionDesignView.vue
+++ vue-js/monstradore/src/views/InteractionDesignView.vue
@@ -2,30 +2,52 @@
   <div v-if="android">
     <v-list>
       <v-list-item
-        :key="i"
-        @click.prevent.stop="menuClick($event, i)"
-        v-for="(i, index) in 25"
+        :key="item"
+        @mousedown.prevent.stop="start($event, item)"
+        @click.prevent.stop="cancel"
+        @mouseout.prevent.stop="cancel"
+        v-for="(item, index) in 25"
         :title="`Dieses Element hat den Index ${index}`"
       />
       <vue-simple-context-menu
         element-id="androidMenu"
+        class="contextMenu"
         :options="options"
         ref="contextMenu"
+        @mouseup.prevent.stop="test"
+        @click.prevent.stop="test"
       />
     </v-list>
   </div>
   <div v-if="ios">
-    <p>iOS</p>
+    <v-list>
+      <v-list-item
+        :key="item"
+        @mousedown.prevent.stop="swipeToDismissStart($event, index)"
+        @mousemove.prevent.stop="swipeToDismiss($event)"
+        @mouseup.prevent.stop="swipeToDismissEnd"
+        v-for="(item, index) in 25"
+        :title="`Dieses Element hat den Index ${index}`"
+      />
+    </v-list>
   </div>
 </template>
 
 <script>
+import VueSimpleContextMenu from "vue-simple-context-menu";
 export default {
   name: "InteractionDesignView",
+  components: {
+    VueSimpleContextMenu,
+  },
   data() {
     return {
       android: false,
-      ios: false,
+      ios: true,
+      longPressTimeOut: null,
+      startPosition: 0,
+      mouseDown: false,
+      deleteIndex: 0,
       options: [
         {
           name: "Delete",
@@ -35,11 +57,35 @@ export default {
     };
   },
   methods: {
-    longPress() {
-      console.log("detect");
+    test() {
+      console.log("clicked");
     },
-    menuClick(event, item) {
-      this.$refs.contextMenu.showMenu(event, item);
+    start(event, item) {
+      this.longPressTimeOut = setTimeout(() => {
+        this.longPressTimeOut = null;
+        this.$refs.contextMenu.showMenu(event, item);
+      }, 500);
+    },
+    cancel() {
+      if (this.longPressTimeOut !== null) {
+        clearTimeout(this.longPressTimeOut);
+        this.longPressTimeOut = null;
+      }
+    },
+    swipeToDismissStart(event, index) {
+      this.startPosition = event.screenX;
+      this.mouseDown = true;
+      this.deleteIndex = index;
+    },
+    swipeToDismiss(event) {
+      if (this.mouseDown && event.screenX - this.startPosition < -50) {
+        console.log("swipe");
+        this.swipeToDismissEnd();
+      }
+    },
+    swipeToDismissEnd() {
+      this.mouseDown = false;
+      this.startPosition = 0;
     },
   },
   mounted() {
@@ -56,4 +102,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import "../../node_modules/vue-simple-context-menu/dist/vue-simple-context-menu.css";
+.contextMenu {
+  margin: 30px !important;
+}
+</style>
